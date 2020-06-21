3211560c6b74ede7620c2702a806cd2dd75836bd
dwayneglevene <dwayneglevene@yahoo.com>
2020-06-21 13:21:30 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# ---- YOUR APP STARTS HERE ----
+#clou# ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template, redirect
+from flask import request
+from model import foodAte
+from datetime import datetime
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -13,4 +14,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html", time=datetime.now())
+
+
+@app.route('/greeting', methods=["POST", "GET"])
+def greeting():
+    if request.method == "POST":
+        user_name = request.form['nickname']
+        thebreakfast = request.form['breakfast']
+        print(user_name)
+        your_nickname=user_name
+        your_breakfast= thebreakfast
+        rateBreakfast = foodAte(your_breakfast)
+        print(your_nickname)
+        return render_template("greeting.html", your_nickname = your_nickname, your_breakfast=your_breakfast,rateBreakfast=rateBreakfast)
+    else:
+        return redirect('/')
+
+@app.route('/owelahs')
+def hello():
+    return "Welcome to Owelahs"
---
+++ model.py
@@ -0,0 +1,5 @@
+def foodAte(food):
+    if food.lower() == "waffles":
+        return "thats a nice breakfast"
+    else:
+        return "Ok sounds good" 
